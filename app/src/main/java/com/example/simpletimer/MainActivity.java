4034c9436f81e40f82253565a7b9a6e30aaf46c4
Jacob Manning <manningjc@g.cofc.edu>
2021-01-28 18:53:43 -0500

    Add to MainActivity

--- app/src/main/java/com/example/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/simpletimer/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.simpletimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button startB;
+    private MyCountDownTimer countDownTimer;
+    private long timerElapsed;
+    private boolean timerHasStarted = false;
+    private TextView text;
+    private TextView timeElapsedView;
+    private final long startTime = 50 * 1000;
+    private final long interval = 1 * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startB = this.findViewById(R.id.button);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(!timerHasStarted) {
+            countDownTimer.start();
+            timerHasStarted = true;
+            String startString = this.getResources().getString(R.string.startLabel);
+            startB.setText(startString);
+        }
+        else {
+            countDownTimer.cancel();
+            timerHasStarted = false;
+            startB.setText(getText(R.string.resetLabel));
+        }
+    }
+
+    public class MyCountDownTimer extends CountDownTimer {
+
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long l) {
+
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
     }
 }
