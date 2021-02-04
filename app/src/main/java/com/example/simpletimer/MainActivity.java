c56b1606d1ae434452051c9dfd525a5734ba2a9a
Jacob Manning <manningjc@g.cofc.edu>
2021-02-04 17:52:14 -0500

    Change string.xml, Add Button layout, Change onClick

--- app/src/main/java/com/example/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/simpletimer/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private Button startB;
     private MyCountDownTimer countDownTimer;
@@ -19,28 +19,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final long startTime = 50 * 1000;
     private final long interval = 1 * 1000;
 
+    private  Button testButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         startB = this.findViewById(R.id.button);
+        startB.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(!timerHasStarted) {
+                    countDownTimer.start();
+                    timerHasStarted = true;
+                    String startString = getString(R.string.startLabel);
+                    startB.setText(startString);
+                }
+                else {
+                    countDownTimer.cancel();
+                    timerHasStarted = false;
+                    startB.setText(getText(R.string.resetLabel));
+                }
+            }
+        });
+
+        /*testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // This Button does this stuff, second Button
+            }
+        });
+        */
     }
 
-    @Override
-    public void onClick(View view) {
-        if(!timerHasStarted) {
-            countDownTimer.start();
-            timerHasStarted = true;
-            String startString = this.getResources().getString(R.string.startLabel);
-            startB.setText(startString);
-        }
-        else {
-            countDownTimer.cancel();
-            timerHasStarted = false;
-            startB.setText(getText(R.string.resetLabel));
-        }
-    }
 
     public class MyCountDownTimer extends CountDownTimer {
 
